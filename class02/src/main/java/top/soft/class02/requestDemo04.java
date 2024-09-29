e4fddb494c12d07c98cbdc2cbf1c4e61c9637e67
ZCH300 <3438287264@qq.com>
2024-09-29 22:50:55 +0800

    javaWeb-learning

--- class02/src/main/java/top/soft/class02/requestDemo04.java
+++ class02/src/main/java/top/soft/class02/requestDemo04.java
@@ -1,5 +1,8 @@
 package top.soft.class02;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -8,51 +11,46 @@ import java.util.Enumeration;
 import java.util.Map;
 import java.util.Set;
 
-/**
- * @author YourName
- * @date 2023/8/16 15:08
- * @description This is a description of the class.
- */
-public class RequestDemo04 extends HelloServlet {
-
+//获取get请求参数
+@WebServlet("/requestDemo04")
+public class requestDemo04 extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // 根据参数名获取参数值
-        String username = request.getParameter("username");
-        System.out.println(username);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //根据参数名获取参数值
+        String username = req.getParameter("username");
+        System.out.println("username="+username);
 
-        // 根据参数名获取参数值的数组
-        String[] hobbies = request.getParameterValues("hobby");
-        if (hobbies != null) {
-            for (String hobby : hobbies) {
-                System.out.println(hobby);
-            }
+        //根据参数名获取参数值的数组
+        String[] hobbies = req.getParameterValues("hobby");
+        for (String hobby : hobbies) {
+            System.out.println("hobby="+hobby);
         }
 
-        // 获取所有参数的参数名称
-        System.out.println("===========");
-        System.out.println("获取所有参数的参数名称");
-        Enumeration<String> parameterNames = request.getParameterNames();
-        while (parameterNames.hasMoreElements()) {
+        //获取所以参数的参数名称
+        System.out.println("======================");
+        System.out.println("获取所有请求的参数名称");
+        Enumeration<String> parameterNames = req.getParameterNames();
+        while (parameterNames.hasMoreElements()){
             String name = parameterNames.nextElement();
-            System.out.println("参数名：" + name);
-            String value = request.getParameter(name);
-            System.out.println(value);
+            System.out.println("参数名"+name);
+            String value = req.getParameter(name);
+            System.out.println("参数值"+value);
         }
 
-        // 获取所有的参数的map集合
-        System.out.println("=================");
-        System.out.println("获取所有的参数的map集合");
-        Map<String, String[]> parameterMap = request.getParameterMap();
-        // 遍历获取
+        //获取说有参数的map集合
+        System.out.println("======================");
+        System.out.println("获取所有请求的map集合");
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        //遍历获取
         Set<String> keySet = parameterMap.keySet();
-        for (String key : keySet) {
-            System.out.println("参数名称：" + key);
-            String[] values = parameterMap.get(key);
+        for (String name : keySet) {
+            System.out.println("参数名称:"+name);
+            String[] values = parameterMap.get(name);
             for (String value : values) {
                 System.out.println(value);
             }
         }
+
+
     }
 }
-
